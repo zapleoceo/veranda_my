8fa99dc949e7a8346ad7915a615aa57cb532448a
zapleoceo <zapleoceo@users.noreply.github.com>
2026-04-08 16:17:44 +0000

    Update Google Analytics tag with real ID G-GQ5X4DSJ79

--- analytics.php
+++ analytics.php
@@ -1,9 +1,9 @@
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-GQ5X4DSJ79"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-XXXXXXXXXX');
+  gtag('config', 'G-GQ5X4DSJ79');
 </script>
