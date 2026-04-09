63b0e33d3443360a8db60f3f1bbd319059fc65e7
zapleoceo <zapleoceo@users.noreply.github.com>
2026-04-09 15:18:45 +0000

    Fix mobile form layout: add min-width: 0 to grid inputs and reduce padding so guests, start time, and duration fit on one line

--- Tr2.php
+++ Tr2.php
@@ -1302,7 +1302,7 @@ if (($_GET['ajax'] ?? '') === 'menu_preorder') {
   <link rel="preconnect" href="https://api.fontshare.com">
   <link rel="preconnect" href="https://cdn.fontshare.com" crossorigin>
   <link href="https://api.fontshare.com/v2/css?f[]=satoshi@400,500,700&f[]=clash-display@500,600&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/assets/css/Tr2.css?v=20260409_0005">
+    <link rel="stylesheet" href="/assets/css/Tr2.css?v=20260409_0006">
 
   <?php include $_SERVER['DOCUMENT_ROOT'] . '/analytics.php'; ?>
 </head>
@@ -1576,6 +1576,6 @@ if (($_GET['ajax'] ?? '') === 'menu_preorder') {
       tableCapsByNum: <?= json_encode($tableCapsByNum, JSON_UNESCAPED_UNICODE) ?>,
     };
   </script>
-  <script src="/assets/js/Tr2.js?v=20260409_0004"></script>
+  <script src="/assets/js/Tr2.js?v=20260409_0006"></script>
 </body>
 </html>
