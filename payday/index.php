d3f14522a8b535e67d8e67d81e089240b2eafbd2
zapleoceo <zapleoceo@users.noreply.github.com>
2026-04-11 12:39:50 +0000

    fix: use account name from API response

--- payday/index.php
+++ payday/index.php
@@ -2767,7 +2767,7 @@ $fmtVnd = function (int $v): string {
     <script src="/assets/user_menu.js" defer></script>
       <?php include $_SERVER['DOCUMENT_ROOT'] . '/analytics.php'; ?>
   <link rel="stylesheet" href="/assets/css/common.css">
-  <link rel="stylesheet" href="/assets/css/payday_index.css?v=20260411_0344">
+  <link rel="stylesheet" href="/assets/css/payday_index.css?v=20260411_0345">
 </head>
 <body>
 <div class="container">
@@ -5398,7 +5398,8 @@ window.__USER_EMAIL__ = <?= json_encode((string)($_SESSION['user_email'] ?? ''),
                 const accMap = {};
                 if (res.accounts) {
                     res.accounts.forEach(a => {
-                        accMap[a.account_id] = a.account_name;
+                        // Poster API finance.getAccounts возвращает account_name
+                        accMap[a.account_id] = a.account_name || a.name;
                     });
                 }
                 
