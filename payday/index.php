9ce3790e687b731beaec34e5631c69e7221b078d
zapleoceo <zapleoceo@users.noreply.github.com>
2026-04-13 12:57:15 +0000

    style(payday): use dark circle for checked switch

--- payday/index.php
+++ payday/index.php
@@ -2824,7 +2824,7 @@ $fmtVnd = function (int $v): string {
     <script src="/assets/user_menu.js" defer></script>
       <?php include $_SERVER['DOCUMENT_ROOT'] . '/analytics.php'; ?>
   <link rel="stylesheet" href="/assets/css/common.css?v=20260412_0170">
-  <link rel="stylesheet" href="/assets/css/payday_index.css?v=20260413_1520">
+  <link rel="stylesheet" href="/assets/css/payday_index.css?v=20260413_1525">
 </head>
 <body>
 <div class="container">
