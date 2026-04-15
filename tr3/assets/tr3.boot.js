cc10c6b8ef280bc778684a2f777818b8ab421ee3
veranda <dev@veranda.local>
2026-04-15 21:55:06 +0700

    TR3: remove mini-loader left margin

--- tr3/assets/tr3.boot.js
+++ tr3/assets/tr3.boot.js
@@ -28,7 +28,7 @@
         soonBookingHours: j.soonBookingHours,
         apiBase: j.apiBase,
       };
-      return loadScript('/tr3/assets/app.js?v=20260416_0125');
+      return loadScript('/tr3/assets/app.js?v=20260416_0131');
     })
     .catch((e) => {
       const msg = document.createElement('div');
