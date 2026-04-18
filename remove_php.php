398caf54a32483a3379c83cf53160b83e0196867
zapleoceo <zapleoceo@users.noreply.github.com>
2026-04-18 21:50:38 +0000

    chore: remove temporary scripts

--- remove_php.php
+++
@@ -1,8 +0,0 @@
-<?php
-$content = file_get_contents('/workspace/admin.php');
-$start = strpos($content, '    $defaultCaps = [');
-$end = strpos($content, '    $saved = $metaRepo->getMany([$resMetaKey');
-if ($start !== false && $end !== false) {
-    $content = substr_replace($content, "\n", $start, $end - $start);
-    file_put_contents('/workspace/admin.php', $content);
-}
