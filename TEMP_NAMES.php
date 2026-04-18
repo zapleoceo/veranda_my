72db847f4fdcca8962682ed856399b83d47e2bf0
zapleoceo <zapleoceo@users.noreply.github.com>
2026-04-18 22:15:59 +0000

    chore: remove unused/garbage files from root (Tr2.php, TableReservation.php, patch_db.php, march.php, sync_history.php, get_token.php, update_pjax.php, etc.)

--- TEMP_NAMES.php
+++
@@ -1,8 +0,0 @@
-<?php
-http_response_code(404);
-header('Content-Type: text/plain; charset=utf-8');
-header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
-header('Pragma: no-cache');
-echo "404 Not Found";
-exit;
-
--- log_viewer.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (file_exists('js_debug.log')) {
-    echo file_get_contents('js_debug.log');
-} else {
-    echo "No log found.";
-}
