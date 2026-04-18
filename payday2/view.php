df07e586fbeaf5638562da8a08e6735ca7c55c9e
zapleoceo <zapleoceo@users.noreply.github.com>
2026-04-18 12:21:15 +0000

    style: remove internal table scroll; let grid columns shrink to min-content and scroll the whole grid instead

--- payday2/view.php
+++ payday2/view.php
@@ -310,7 +310,7 @@ if (count($posterAccountsById) > 0) {
 $fmtVnd = function (int $v): string {
     return number_format($v, 0, '.', "\u{202F}");
 };
-$payday2AssetVersion = '20260417_5500';
+$payday2AssetVersion = '20260417_5600';
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -324,7 +324,7 @@ $payday2AssetVersion = '20260417_5500';
     <script src="/assets/user_menu.js" defer></script>
       <?php include $_SERVER['DOCUMENT_ROOT'] . '/analytics.php'; ?>
   <link rel="stylesheet" href="/assets/css/common.css?v=20260412_0171">
-  <link rel="stylesheet" href="/assets/css/payday_index.css?v=20260417_5500">
+  <link rel="stylesheet" href="/assets/css/payday_index.css?v=20260417_5600">
   <link rel="stylesheet" href="/payday2/assets/css/payday2.css?v=<?= htmlspecialchars($payday2AssetVersion) ?>">
  </head>
 <body>
