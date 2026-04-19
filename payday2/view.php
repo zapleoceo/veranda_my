09186efaa76acd63df2cd6f3f0f6efe08401bc53
zapleoceo <zapleoceo@users.noreply.github.com>
2026-04-19 11:52:05 +0000

    fix: correct image paths for KashShift and Supplies icons to /payday2/img/

--- payday2/view.php
+++ payday2/view.php
@@ -356,7 +356,7 @@ if (count($posterAccountsById) > 0) {
 $fmtVnd = function (int $val): string { return FinanceHelper::fmtVnd($val); };
 $fmtVndCents = function (int $cents): string { return FinanceHelper::fmtVndCents($cents); };
 $payday2CsrfToken = payday2_ensure_csrf();
-$payday2AssetVersion = '20260419_0022';
+$payday2AssetVersion = '20260419_0023';
 $payday2ClientConfig = [
     'userEmail' => (string)($_SESSION['user_email'] ?? ''),
     'csrfToken' => $payday2CsrfToken,
@@ -400,10 +400,10 @@ $payday2ConfigJsonFlags = JSON_UNESCAPED_UNICODE | JSON_HEX_TAG | JSON_HEX_AMP |
                 <button type="button" class="tab" id="tabOut">OUT</button>
                 <div class="pd2-ml-auto pd2-d-flex">
                     <button type="button" class="tab pd2-tab-gold pd2-d-flex pd2-align-center pd2-justify-center" id="btnKashShift" style="padding: 4px 10px; background: #f97316;" title="KashShift">
-                        <img src="/img/Cash.png" alt="KashShift" style="width: 20px; height: 20px; object-fit: contain;">
+                        <img src="/payday2/img/Cash.png" alt="KashShift" style="width: 20px; height: 20px; object-fit: contain;">
                     </button>
                     <button type="button" class="tab pd2-tab-gold pd2-ml-5 pd2-d-flex pd2-align-center pd2-justify-center" id="btnSupplies" style="padding: 4px 10px; background: #f97316;" title="Supplies">
-                        <img src="/img/Supply.png" alt="Supplies" style="width: 20px; height: 20px; object-fit: contain;">
+                        <img src="/payday2/img/Supply.png" alt="Supplies" style="width: 20px; height: 20px; object-fit: contain;">
                     </button>
                 </div>
             </div>
