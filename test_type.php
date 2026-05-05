9fe39404cca832456f2848150e1f96cafd6ea7ac
zapleoceo <zapleoceo@users.noreply.github.com>
2026-05-05 05:31:14 +0000

    chore: remove unused root test files

--- test_type.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Test mapping
-$rawType = 10;
-$type = $rawType;
-if ($rawType === 10) $type = 4;
-elseif ($rawType === 8) $type = 5;
-elseif ($rawType === 2 || $rawType === 3) $type = 1;
-echo "raw: $rawType, new: $type\n";
