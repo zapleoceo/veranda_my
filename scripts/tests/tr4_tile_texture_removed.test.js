fa997155747e74b53cc1e16594d49a25ef8feca2
zapleoceo <zapleoceo@users.noreply.github.com>
2026-05-08 16:22:23 +0000

    ui(tr4): remove tile texture background

---
+++ scripts/tests/tr4_tile_texture_removed.test.js
@@ -0,0 +1,12 @@
+const assert = (cond, msg) => {
+  if (!cond) {
+    process.stderr.write(`FAIL: ${msg}\n`)
+    process.exit(1)
+  }
+}
+
+const fs = require('fs')
+const css = fs.readFileSync('/workspace/tr4/assets/layout.css', 'utf8')
+assert(!/gray-tiles-texture\.jpg/.test(css), 'tile-layer must not use gray-tiles-texture.jpg')
+process.stdout.write('OK\n')
+
